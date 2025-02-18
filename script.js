fe8b7c70c2c330f4f85e8c11a6c1cc9d6ef81458
ricboto <ricbot1988@gmail.com>
2025-02-18 17:53:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const images = [
+    "slides/slide1.jpg",
+    "slides/slide2.jpg",
+    "slides/slide3.jpg"
+];
 
+let index = 0;
+const slide = document.getElementById("slide");
+
+function changeSlide() {
+    index = (index + 1) % images.length;
+    slide.src = images[index];
+}
+
+setInterval(changeSlide, 3000); // Change slide every 3 seconds
